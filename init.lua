fc5192616afd0e2216e7666b40099592e76fac05
Anup Mahindre <anup.mahindre.7@gmail.com>
2023-08-15 07:44:50 +0530

    Add clangd-extensions support

--- init.lua
+++ init.lua
@@ -119,6 +119,9 @@ require('lazy').setup({
   },
 
   {
+    'p00f/clangd_extensions.nvim'
+  },
+  {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
     dependencies = {
@@ -572,6 +575,9 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
+
+  require("clangd_extensions.inlay_hints").setup_autocmd()
+  require("clangd_extensions.inlay_hints").set_inlay_hints()
 end
 
 -- document existing key chains
